af10007e41019818ebb3e142be61f20292e93c8d
Nenad Mitkovic <nenadmit@outlook.com>
2020-06-09 12:28:22 +0000

    Feature/application skeleton

---
+++ src/main/java/com/internship/tabulaprocessing/TabulaProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.internship.tabulaprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabulaProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabulaProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/internship/tabulaprocessing/TabulaProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.internship.tabulaprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabulaProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
