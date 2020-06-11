2b852b1ac6474c24bd93a1fd4ea62ca790f49c83
Elizaa11 <eliza.petrova11@gmail.com>
2020-06-11 11:06:12 +0000

    Update src/main/java/com/internship/tabulaprocessing/TabulaProcessingApplication.java

--- src/main/java/com/internship/tabulaprocessing/TabulaProcessingApplication.java
+++ src/main/java/com/internship/tabulaprocessing/TabulaProcessingApplication.java
@@ -1,12 +1,7 @@
 package com.internship.tabulaprocessing;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TabulaProcessingApplication {
