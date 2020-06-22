925b4c97fa41e7ed60e12d8a591f7a2539cd21b1
Nenad Mitkovic <nenadmit@outlook.com>
2020-06-22 15:07:33 +0200

    modified queryParameter

--- src/main/java/com/internship/tabulaprocessing/controller/QueryParameter.java
+++ src/main/java/com/internship/tabulaprocessing/controller/QueryParameter.java
@@ -9,12 +9,13 @@ import javax.validation.constraints.Min;
 
 public class QueryParameter {
 
-    @Min(value = 1,message = "Page size cannot be less than 1")
-    @Max(value = 100,message = "Page size cannot be greater than 100")
+    @Min(value = 1, message = "Page size cannot be less than 1")
+    @Max(value = 100, message = "Page size cannot be greater than 100")
     private int size = 15;
-    @Min(value = 1,message = "Page cannot be less than 1")
+    @Min(value = 1, message = "Page cannot be less than 1")
     private int page = 1;
     private String sortBy = "id";
+    private boolean desc;
 
     public int getSize() {
         return size;
@@ -40,7 +41,16 @@ public class QueryParameter {
         this.sortBy = sortBy;
     }
 
+    public boolean isDesc() {
+        return desc;
+    }
+
+    public void setDesc(boolean desc) {
+        this.desc = desc;
+    }
+
     public Pageable getPageable() {
-        return PageRequest.of(page - 1, size, Sort.by(sortBy).ascending());
+        return desc ? PageRequest.of(page - 1, size, Sort.by(sortBy).ascending())
+                : PageRequest.of(page - 1, size, Sort.by(sortBy).descending());
     }
 }
