82e39e55c104ba94080bc4218b09d81dbf1bd486
Nenad Mitkovic <nenadmit@outlook.com>
2020-06-26 11:55:23 +0000

    Fix/entity table names to lowercase

--- src/main/java/com/internship/tabulacore/entity/Account.java
+++ src/main/java/com/internship/tabulacore/entity/Account.java
@@ -4,8 +4,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -15,6 +13,7 @@ import java.util.Set;
 
 @Data
 @Entity
+@Table(name = "account")
 @EntityListeners(AuditingEntityListener.class)
 public class Account {
 
@@ -22,16 +21,17 @@ public class Account {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
+  @Column(name = "full_name")
   private String fullName;
 
   private String email;
 
   private String password;
 
-  @CreatedDate
+  @Column(name = "datetime_created")
   private LocalDateTime datetimeCreated;
 
-  @LastModifiedDate
+  @Column(name = "datetime_updated")
   private LocalDateTime datetimeUpdated;
 
   @EqualsAndHashCode.Exclude
